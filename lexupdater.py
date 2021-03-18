8651961cafd30ac99bd8fa0ee45659cac06e8970
Per Erik Solberg <per.solberg@nb.no>
2021-03-18 13:10:35 +0100

    fix issue #1: use Argparse

--- lexupdater.py
+++ lexupdater.py
@@ -3,17 +3,30 @@
 
 from bin import DatabaseUpdater
 from config import dialects, word_table, database, rules, blacklists
+import argparse
+import datetime
+
+begin_time = datetime.datetime.now()
+
+# Argument parser
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--print_dialects', '-d', action='store', type=str, nargs='*', default=dialects, help='Generate lexicon files for one or more specified dialects.')
+parser.add_argument('--print_base', '-b', action='store_true', help='Generate a base lexicon file, containing the state of the lexicon prior to updates.')
+args = parser.parse_args()
+
+
 
 # Function to select the state of the lexicon before the updates
 
-def get_base(connection, dialects):
+def get_base(connection):
     stmt = """SELECT w.word_id, w.wordform, w.pos, w.feats, w.source, w.decomp_ort, w.decomp_pos, 
                 w.garbage, w.domain, w.abbr, w.set_name, w.style_status, w.inflector_role, w.inflector_rule, 
                 w.morph_label, w.compounder_code, w.update_info, p.pron_id, p.nofabet, p.certainty
                 FROM words w LEFT JOIN base p ON p.word_id = w.word_id;"""
     cursor = connection.cursor()
     result = cursor.execute(stmt).fetchall()
-    return {d:result for d in dialects}
+    return result
 
 
 
@@ -23,17 +36,24 @@ def get_base(connection, dialects):
 
 updateobj = DatabaseUpdater(database, rules, dialects, word_table, blacklists=blacklists)
 connection = updateobj.get_connection()
-base = get_base(connection, dialects)
 updateobj.update()
 exp = updateobj.get_results()
 updateobj.close_connection()
 
+print(datetime.datetime.now() - begin_time)
+
 # Write a base and exp lexicon file for each dialect area d.
 
-for d in ['e_spoken']:
-    with open(f'{d}_base.txt', 'w') as basefile, open(f'{d}_exp.txt', 'w') as expfile:
-        for el in base[d]:
-            basefile.write(f'{el[1]}\t{el[2]}\t{el[3]}\t{el[-2]}\n')
+
+
+for d in args.print_dialects:
+    with open(f'{d}.txt', 'w') as expfile:
         for elm in exp[d]:
             expfile.write(f'{elm[1]}\t{elm[2]}\t{elm[3]}\t{elm[-2]}\n')
+    if args.print_base:
+        base = get_base(connection)
+        with open(f'base.txt', 'w') as basefile:
+            for el in base:
+                basefile.write(f'{el[1]}\t{el[2]}\t{el[3]}\t{el[-2]}\n')
 
+print(datetime.datetime.now() - begin_time)
