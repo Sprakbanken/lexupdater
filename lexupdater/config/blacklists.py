7deeb834f706d57a17141976ab638b52ec430de0
Ingerid Dale <ingerid.dale@nb.no>
2021-04-15 14:48:44 +0200

    refactor: folder restructuring
    BREAKING CHANGE: module imports will not work the same as before

---
+++ lexupdater/config/blacklists.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+# Blacklists contains list of words which should not be affected by given replacement rulesets.
+
+blacklist1 = {'ruleset': 
+                'retrotest', 
+            'words': 
+                ['garn', 'klarne']
+            }
+
+blacklist2 = {'ruleset': 'masc', 
+                'words': 
+                    ['søknader', 'søknadene', 'dugnader', 'dugnadene']
+            }
