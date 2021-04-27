679dff7f673f38ac446490da52fd81750de4c2fe
Per Erik Solberg <per.solberg@nb.no>
2021-04-27 11:23:50 +0200

    Update lexupdater/__main__.py
    Co-authored-by: ingerid <37677043+Ingerid@users.noreply.github.com>

--- lexupdater/__main__.py
+++ lexupdater/__main__.py
@@ -15,14 +15,25 @@ begin_time = datetime.datetime.now()
 # Argument parser
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--print_dialects", "-d", action="store", type=str,
-                    nargs="*", default=dialects,
-                    help=("Generate lexicon files for"
-                          " one or more specified dialects."))
-parser.add_argument("--print_base", "-b", action="store_true",
-                    help=("Generate a base lexicon file,"
-                          "containing the state of the "
-                          "lexicon prior to updates."))
+parser.add_argument(
+    "--print_dialects",
+    "-d",
+    action="store",
+    type=str,
+    nargs="*",
+    default=dialects,
+    help=("Generate lexicon files for" " one or more specified dialects."),
+)
+parser.add_argument(
+    "--print_base",
+    "-b",
+    action="store_true",
+    help=(
+        "Generate a base lexicon file,"
+        "containing the state of the "
+        "lexicon prior to updates."
+    ),
+)
 args = parser.parse_args()
 
 
