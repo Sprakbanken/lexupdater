85797773a0db29c026966a67756ce6c49571408c
Ingerid Dale <ingerid.dale@nb.no>
2021-08-19 01:21:34 +0200

    feat: separate the different CLI operations into subcommands
    * fix: skip files that don't exist when converting
    * tests: update main script tests
    
    BREAKING CHANGE: new CLI command signatures

--- tests/test_lexupdater.py
+++ tests/test_lexupdater.py
@@ -22,7 +22,7 @@ def test_main_default_update(all_dialects, tmp_path):
     # Use default values, except database and output directory
     result = runner.invoke(
         lexupdater.main,
-        f"-o {str(output_dir)} --db tests/dummy_data.db"
+        f"-o {str(output_dir)} -db tests/dummy_data.db"
     )
     result_files = [
         file_path.name for file_path in output_dir.iterdir()
@@ -30,20 +30,19 @@ def test_main_default_update(all_dialects, tmp_path):
     # then
     assert result.exit_code == 0
     assert result_files != []
-    for file in result_files:
-        assert file.endswith(".txt")
     assert all([file in result_files for file in expected_update_files])
 
 
 @pytest.mark.parametrize(
-    "cli_arg,expected_files",
+    "sub_command,expected_file",
     [
-        ["-b", ("base.txt", "updated_lexicon_n_written.txt")],
-        ["-m", ("words_matching_rules_n_written.txt",)],
-    ],
-    ids=["base", "match"]
+        ["base", "base.txt"],
+        ["match", "words_matching_rules_n_written.txt"],
+        ["insert", "base_new_words.txt"],
+        ["update", "updated_lexicon_n_written.txt"],
+    ]
 )
-def test_script_with_config_and_cli_args(cli_arg, expected_files, tmp_path):
+def test_subcommands(sub_command, expected_file, tmp_path):
     # given
     output_dir = (tmp_path / "dummy_output")
     output_dir.mkdir()
@@ -52,9 +51,28 @@ def test_script_with_config_and_cli_args(cli_arg, expected_files, tmp_path):
     # when
     result = runner.invoke(
         lexupdater.main,
-        f"{cli_arg} -v -c tests/dummy_config.py -o {output_dir}"
+        f"-c tests/dummy_config.py -o {str(output_dir)} {sub_command}"
     )
     result_files = [file_path.name for file_path in output_dir.iterdir()]
     # then
     assert result.exit_code == 0
-    assert all([file in result_files for file in expected_files])
+    assert expected_file in result_files
+
+
+def test_convert_formats(tmp_path):
+    output_dir = (tmp_path / "dummy_output")
+    output_dir.mkdir()
+
+    (output_dir / "updated_lexicon_n_written.txt").write_text(
+        "-elser	NN	PLU|IND|NOM|NEU-MAS-FEM	EH1 L S AA0 R")
+    expected_file = "NST_nob_n_written.dict"
+    runner = CliRunner()
+    # when
+    result = runner.invoke(
+        lexupdater.main,
+        f"-c tests/dummy_config.py -o {str(output_dir)} convert"
+    )
+    result_files = [file_path.name for file_path in output_dir.iterdir()]
+    # then
+    assert result.exit_code == 0
+    assert expected_file in result_files
