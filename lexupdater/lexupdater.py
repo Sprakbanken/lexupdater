48a88e9d9a7357a9c8b8494a9d386a58b3c9d97d
Ingerid Dale <ingerid.dale@nb.no>
2021-10-21 14:22:13 +0200

    fix: compare command only writes the comparison file to disk

--- lexupdater/lexupdater.py
+++ lexupdater/lexupdater.py
@@ -468,11 +468,6 @@ def compare_matching_updated_transcriptions(
     comparison = compare_transcriptions(matching_words, updated_words)
     now = datetime.now().strftime("%Y-%m-%d_%H%M")
     comparison.to_csv(output_dir / f"comparison_{now}.txt")
-    # save updates to files and convert them
-    write_lex_per_dialect(updated_words, output_dir, LEX_PREFIX, strip_ids)
-    convert_lex_to_mfa(
-        lex_dir=output_dir,
-        combine_dialect_forms=True)
 
 
 @main.command("insert")
