4e5aaea296107d46a8d4088199ebd70b15e19aaf
Ingerid Dale <ingerid.dale@nb.no>
2023-02-23 10:32:06 +0100

    feat: add RuleObj and parse_rulesets
    Signed-off-by: Ingerid Dale <ingerid.dale@nb.no>

--- lexupdater/dialect_updater.py
+++ lexupdater/dialect_updater.py
@@ -3,16 +3,25 @@
 Parse their constraints and exemptions
 into variables to fill slots in SQL query templates.
 """
-import functools
+
 import logging
 from typing import List, Generator
 
-from .constants import ruleset_schema, exemption_schema, COL_WORDFORM
+from .constants import ruleset_schema, COL_WORDFORM, rule_schema
 from .utils import filter_list_by_list, validate_objects, add_placeholders
-from .rule_objects import map_rule_exemptions
+from .rule_objects import RuleObj
 
 
-def parse_constraints(constraints: List):
+def sql_operator(is_regex):
+    return "REGEXP" if is_regex else "="
+
+
+def parse_constraint(constraint) -> tuple:
+    """Define SQL statement fragment to follow 'WHERE' """
+    return f"{constraint.field} {sql_operator(constraint.is_regex)} ?", constraint.pattern
+
+
+def parse_constraints(constraints: List) -> List[tuple]:
     """Construct SQL WHERE queries from the replacement rule constraints.
 
     Grammatical categories and features that are given in the word table of
@@ -30,13 +39,7 @@ def parse_constraints(constraints: List):
         SQL clause fragment and list of feature values
         for the words that the rule applies to
     """
-    values = []
-    constraint_fragments = []
-    for const in constraints:
-        operator = "=" if not const["is_regex"] else "REGEXP"
-        constraint_fragments.append(f"{const['field']} {operator} ?")
-        values.append(const["pattern"])
-    return constraint_fragments, values
+    return [parse_constraint(c) for c in constraints]
 
 
 def parse_exemptions(exemption_words):
@@ -53,76 +56,36 @@ def parse_exemptions(exemption_words):
         SQL fragment and a list of words that are exempt
     """
     if exemption_words:
-        return f"{COL_WORDFORM} NOT IN ({add_placeholders(exemption_words)})"
+        return f"{COL_WORDFORM} NOT IN ({add_placeholders(exemption_words)})", exemption_words
     return ""
 
 
-def parse_conditions(constraints: list, exempt_words: list) -> tuple:
+def parse_conditions(constraints: list, exempt_words: list, prefix) -> tuple:
     """Create an SQL WHERE-query fragment based on constraints and exemptions.
 
     The conditional fragment is meant to be inserted in
     a SELECT or UPDATE query, along with the values
     that fill the placeholder slots.
     """
-    is_constrained = bool(constraints)
-    if not is_constrained and not exempt_words:
+    if not bool(constraints) and not exempt_words:
         return "", []
 
-    constraint_strs, constraint_values = parse_constraints(constraints)
-    exempt_str = parse_exemptions(exempt_words)
+    constraints, cond_vals = parse_constraints(constraints)
+    exemption, exempt_words = parse_exemptions(exempt_words)
 
-    conditions = [string for string in (*constraint_strs, exempt_str) if string]
-    values = constraint_values + exempt_words
-    logging.debug("Conditions: %s ", conditions)
-    logging.debug("Values: %s", values)
-    return conditions, values
+    coordinated_conditions = f"{prefix} {' AND '.join(c for c in constraints+exemption) if c}"
+    return coordinated_conditions, cond_vals + exempt_words
 
-
-def parse_rules(
-    filter_dialects: list,
-    rulesets: list,
-    exemptions: list,
-) -> Generator:
-    """Parse rulesets, and yield the values needed to construct SQL queries.
+def parse_rulesets(rulesets: list, exemptions) -> Generator:
+    """Parse a list of ruleset dicts and exemptions, yield rule objects with attributes for constructing SQL queries.
 
     Yields
     ------
-    tuple
-        str: dialect affected by the rule,
-        str: regex pattern,
-        str: replacement string,
-        str: conditional query fragment,
-        list: conditional values to fill placeholders
+    RuleObj
     """
-    rule_exemptions = map_rule_exemptions(
-        validate_objects(exemptions, exemption_schema)
-    )
-    validated_rules = validate_objects(rulesets, ruleset_schema)
-
-    for ruleset in validated_rules:
-        rule_dialects = filter_list_by_list(ruleset["areas"], filter_dialects)
-        if not rule_dialects:
-            continue
-        logging.debug(
-            "Parsing rule set '%s' for dialects %s",
-            ruleset.get("name"),
-            ", ".join(rule_dialects)
-        )
-        exempt_words = rule_exemptions.get(ruleset["name"], [])
-
-        for rule in ruleset["rules"]:
-            logging.debug("Rule pattern: %s", rule["pattern"])
-            logging.debug("Rule replacement: %s", rule["replacement"])
-            cond_string, cond_values = parse_conditions(
-                rule["constraints"], exempt_words
-            )
-            for dialect in rule_dialects:
-                print(".", end="")
-                yield (
-                    dialect,
-                    rule["pattern"],
-                    rule["replacement"],
-                    cond_string,
-                    cond_values
-                )
-    print("")
+    for ruleset in rulesets:
+        name = ruleset.get("name")
+        for dialect in ruleset.get("areas"):
+            for idx, rule in enumerate(ruleset.get("rules")):
+                ruleobject = RuleObj(**rule, ruleset=name, dialect=dialect,exemptions=exemptions.get(name), idx=idx)
+                yield ruleobject
