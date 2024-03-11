8ee16f8610b3c46bd89c1b8cfd7a52aa738302db
Ingerid Dale <ingerid.dale@nb.no>
2024-03-11 15:52:39 +0100

    rename download script and ignore data files

---
+++ fetch_data.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+#GITHUB_TOKEN="${1:-$GITHUB_TOKEN}"
+
+##### Define functions to fetch files with git ###############################
+
+function fetch_conversion_module {
+  git remote add conversion git@github.com:Sprakbanken/convert_nofabet.git
+  git fetch conversion
+  git show conversion/main:conversion.py > lexupdater/conversion.py
+  git remote remove conversion
+}
+
+function fetch_rules_exemptions {
+  git remote add nb_uttale git@github.com:Sprakbanken/nb_uttale.git
+  git fetch nb_uttale
+  git show nb_uttale/main:data/input/rules_v1.py > rules.py
+  git show nb_uttale/main:data/input/exemptions_v1.py > exemptions.py
+  git show nb_uttale/main:data/input/newwords_2022.csv > newwords.csv
+  git remote remove nb_uttale
+}
+
+function fetch_lexicon {
+  wget https://www.nb.no/sbfil/uttaleleksikon/nst_lexicon_bm.db
+}
+
+################### RUN FUNCTIONS ###################
+
+#fetch_conversion_module
+fetch_lexicon
+fetch_rules_exemptions
